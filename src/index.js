f47cb0f88817c1f1c242eddc685873bd67f2a481
Gleb <glebshm@rambler.ru>
2024-08-31 21:19:09 +0300

    complete morsedecoder

--- src/index.js
+++ src/index.js
@@ -38,8 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr=[]
+    for (var i = 0; i < expr.length; i+=10) {
+        arr.push(expr.slice(i, i+10))
+    }
+    let result=[]
+    for (let i = 0; i < arr.length; i++){
+        let subarr=[]
+        for (let j = 0; j <= arr[i].length; j+=2) {
+            if(arr[i][j]+arr[i][j+1]=='10') {
+                subarr.push('.')
+            }else if(arr[i][j]+arr[i][j+1]=='11') {
+                subarr.push('-')
+            }
+        }
+        result.push(subarr)
+    }
+    
+    return result.map(i=>MORSE_TABLE[i.join('')]||' ').join('')
 }
+console.log(decode('00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010'))
+
 
 module.exports = {
     decode
